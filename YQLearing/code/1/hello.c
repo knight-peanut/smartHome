6fe1e7f4c83840f9da96aca11b797e549be8dbd4
knight-peanut <212395159@qq.com>
2020-07-05 14:27:48 +0800

    Create hello.c

---
+++ YQLearing/code/1/hello.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{	
+								//花括号要成对打
+	printf("hello world\n");	//要有缩进 用tab键进行缩进,写代码的时候让代码有层次感
+	
+	return 0;
+}					
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
